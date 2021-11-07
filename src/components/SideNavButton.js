547867888a1ef4b4be00dea1f578672fb179d14a
Caiobkw <Caiobkw@users.noreply.github.com>
2021-11-07 19:22:41 -0300

    Updates

--- src/components/SideNavButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import hamburgermenu from '../resources/vectors/hamburger-menu.svg'
-
-export const SideNavButton = props => {
-    
-    
-
-    return <img src={hamburgermenu} className='sideNavButton' onClick={props.onClick} style={props.style} ></img>;
-}
